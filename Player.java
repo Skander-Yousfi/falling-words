ed6cbc63c2de4b11e9465a4b24855585d889a129
Carolina-stav <81708703+Carolina-stav@users.noreply.github.com>
2021-11-01 15:01:01 +0100

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,33 @@
+package projet;
+
+public class Player {
+	private String pseudo;
+	private int health = 3;
+
+	public Player(String pseudo) {
+		this.pseudo=pseudo;
+	}
+	
+	public boolean verif_health() {
+		if (this.health > 0) {
+			return true;
+		}
+		return false ;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+	
+	public void looseHealth() {
+		this.health=(this.health)-1;
+	}
+	
+	public static void main(String[] args) {
+		Player p= new Player("jean");
+		p.health=2;
+		p.looseHealth();
+		System.out.println(p.verif_health());
+		System.out.println(p.getHealth());
+	}
+}
