560846933ed07608ae6a69aec20a01ddda83ed2b
Romane-kulesza <74608881+Romane-kulesza@users.noreply.github.com>
2021-11-04 11:16:31 +0100

    Update Partie.java

--- Partie.java
+++ Partie.java
@@ -10,22 +10,22 @@ public class Partie {
 	final int LOWER=10; // ordonnee la plus basse a ne pas depasser 
 	
 	
-	public void start() {
-		// a completer, on initialise tous les parametres de la partie avec les données entrés par l'utilisateur 
+	public Partie() {
+		// a completer, on initialise tous les parametres de la partie avec les donnÃ©es entrÃ©s par l'utilisateur 
 		
 	}
 	
 	public void end() {
-        // a completer, la partie est terminée, on est sortie du while verifiant le nbr de vie restant, on affiche le temps de jeu, je score, on sauvegarde le score etc...
+        // a completer, la partie est terminÃ©e, on est sortie du while verifiant le nbr de vie restant, on affiche le temps de jeu, je score, on sauvegarde le score etc...
 		//appel a la methode sauvegarde etc...
 	}
 	
 	
-	public void verif_row(){ //ou alors renvoit un int (1 si partie continue et 0 si partie terminée)  a verifier dans boucle while et plus besoin du verif_health ?
+	public void verif_row(){ //ou alors renvoit un int (1 si partie continue et 0 si partie terminÃ©e)  a verifier dans boucle while et plus besoin du verif_health ?
 	    for (Words w : this.screenWords) {
 	    	Coord coo = w.getCoord();
 	    	if (coo.getY() < LOWER) { //si un des mots de la liste a l'ecran est plus bas que LOWER
-	    		this.player.looseHealth(); //on enlève une vie 
+	    		this.player.looseHealth(); //on enlÃ¨ve une vie 
 	    	}
 	    	//else      //c'est ici qu'on fait descendre les mots ? 
 	    	//	w.(coord.down);
