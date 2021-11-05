4d2149e4d95b8e2fbe029d4152d14ae192d39d87
Carolina-stav <81708703+Carolina-stav@users.noreply.github.com>
2021-11-05 11:10:50 +0100

    Update Partie.java

--- Partie.java
+++ Partie.java
@@ -8,29 +8,47 @@ public class Partie {
 	private int compteur; // ??
 	private ArrayList<Words> screenWords;
 	final int LOWER=10; // ordonnee la plus basse a ne pas depasser 
+	private int index=0; //pour parcourir notre tableau de base de mots 
+	private JFrame fenetre;
 	
 	
-	public Partie() {
-		// a completer, on initialise tous les parametres de la partie avec les données entrés par l'utilisateur 
-		
+	public Partie(String ps) throws IOException {
+		compteur=0;
+		list= new WordList();
+		player = new Player(ps);
+		screenWords= new ArrayList<Words>();
+		fenetre = new FenetreJeu(screenWords);
 	}
 	
 	public void end() {
+		//avec swing et mothode sauvegarde 
         // a completer, la partie est terminée, on est sortie du while verifiant le nbr de vie restant, on affiche le temps de jeu, je score, on sauvegarde le score etc...
 		//appel a la methode sauvegarde etc...
 	}
 	
 	
-	public void verif_row(){ //ou alors renvoit un int (1 si partie continue et 0 si partie terminée)  a verifier dans boucle while et plus besoin du verif_health ?
-	    for (Words w : this.screenWords) {
+	public void updatePosition(){ //ou alors renvoit un int (1 si partie continue et 0 si partie terminée)  a verifier dans boucle while et plus besoin du verif_health ?
+		if (compteur==4){
+	    		if( index==(list.getWords().size())-1) {
+	    			list.shuffle();
+	    			index=0;
+	    		}
+	    		Words word = list.getWords().get(index);
+	    		screenWords.add(word);
+	    		compteur=0;
+	    }
+		for (Words w : this.screenWords) {
+	    	w.downWord();
 	    	Coord coo = w.getCoord();
 	    	if (coo.getY() < LOWER) { //si un des mots de la liste a l'ecran est plus bas que LOWER
-	    		this.player.looseHealth(); //on enlève une vie 
+	    	this.player.looseHealth(); //on enlève une vie 
+	    	screenWords.remove(w);
+	    	compteur++;
 	    	}
-	    	//else      //c'est ici qu'on fait descendre les mots ? 
-	    	//	w.(coord.down);
 	    }
-	}
+		
+		fenetre.update(screenWords);
+	    }
 	
 	
 	
