6121786c83453684232c0c43eab2477a0eb0ee69
Romane-kulesza <74608881+Romane-kulesza@users.noreply.github.com>
2021-11-11 18:47:37 +0100

    Update FenetreJeu.java

--- FenetreJeu.java
+++ FenetreJeu.java
@@ -11,13 +11,23 @@ public class FenetreJeu extends JFrame {
 	private JPanel pane;
 	private GridBagConstraints c;
 	
-	public FenetreJeu(ArrayList<Words> l) {
+	public FenetreJeu(ArrayList<Words> l, Player p) {
 		super("Falling Words");
-		setSize(400, 300);
+		setSize(450, 400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		panel = new JPanel(); 
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		s = new JPanel();
+		score = new JLabel("Nombre de vies : 0");
+		score.setMaximumSize(new Dimension(400, 50));
+		s.add(score);
+		panel.add(s, Component.CENTER_ALIGNMENT);
+		h = new JPanel();
+		health = new JLabel("Nombre de vies : "+p.getHealth());
+		health.setMaximumSize(new Dimension(400, 50));
+		h.add(health);
+		panel.add(h, Component.CENTER_ALIGNMENT);
 		pane = new JPanel();
 		pane.setLayout(new GridBagLayout());
 		c = new GridBagConstraints();
@@ -27,13 +37,21 @@ public class FenetreJeu extends JFrame {
 				JLabel t = new JLabel(w.getWord());
 				c.gridx = w.getCoord().getX();
 				c.gridy = w.getCoord().getY();
-//				t.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.red),pane.getBorder()));
+				if (w.getWord()!="                     ") {
+					t.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.red),pane.getBorder()));
+
+				}
+				else {
+					t.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.lightGray),pane.getBorder()));
+
+				}
 				pane.add(t, c); 
 			}
 		}
+		pane.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.lightGray),pane.getBorder()));
+		pane.setBackground(Color.lightGray);
 		panel.add(pane, Component.CENTER_ALIGNMENT);
-//		pane.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.red),pane.getBorder()));
-		text = new JTextField("Ecrivez ici !");
+		text = new JTextField("");
 		text.setMaximumSize(new Dimension(400, 50));
 		panel.add(text, Component.CENTER_ALIGNMENT);
 		setContentPane(panel);
@@ -45,15 +63,15 @@ public class FenetreJeu extends JFrame {
 		Words[][] list = new Words[5][15];
 		for (int i=0; i<5; i++) {
 			for (int j = 0; j<15; j++) {
-				list[i][j] = new Words("                  ", i, j);
+				list[i][j] = new Words("                     ", i, j);
 			}
 		}
 		for (Words w : l) {
-			list[w.getCoord().getY()][w.getCoord().getX()]=w;
+			list[w.getCoord().getX()][w.getCoord().getY()]=w;
 		}
 		return(list);
 	}
-	public void update(ArrayList<Words> l) {
+	public void updateScreen(ArrayList<Words> l) {
 		pane.removeAll();
 		Words[][] list = createTab(l);
 		for (int i = 0; i<5; i++) {
@@ -61,11 +79,28 @@ public class FenetreJeu extends JFrame {
 				JLabel t = new JLabel(w.getWord());
 				c.gridx = w.getCoord().getX();
 				c.gridy = w.getCoord().getY();
-//				t.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.red),pane.getBorder()));
+				if (w.getWord()!="                     ") {
+//					t.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.red),pane.getBorder()));
+					t.setBackground(Color.gray);
+				}
+				else {
+					t.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.lightGray),pane.getBorder()));
+
+				}
 				pane.add(t, c); 
 			}
 		}
 		pane.validate();
 		pane.repaint();
+	}
+	public void updateHealth(Player p) {
+		health.setText("Nombre de vies : "+p.getHealth());
+		h.validate();
+		h.repaint();
+	}
+	public void updateScore(int score) {
+		this.score.setText("Score : "+ score);
+		s.validate();
+		s.repaint();
 	}
 }
