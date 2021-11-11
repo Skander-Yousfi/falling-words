1f9eb0a4551683d59bf12f37627dff1175885cb8
Romane-kulesza <74608881+Romane-kulesza@users.noreply.github.com>
2021-11-11 19:04:27 +0100

    Update Words.java

--- Words.java
+++ Words.java
@@ -9,45 +9,35 @@ public class Words {
 	
 	public Words (String mot) {
 		this.word = mot;
-		if (mot.length()>6) {
-			this.points = 2;
-		}
-		else {
-			this.points = 1;
-		}
+		this.points = mot.length();
 		Random random = new Random();
 		this.coord = new Coord(random.nextInt(5), 0);
 	}
-	// Méthode pour créer un mot ou l'on veut sur l'écran : utile pour vérifications mais pas pour le projet en soi.
+	
 	public Words (String mot, int x, int y) {
 		this.word = mot;
-		if (mot.length()>6) {
-			this.points = 2;
-		}
-		else {
-			this.points = 1;
-		}
+		this.points = mot.length();
 		this.coord = new Coord(x,y);
 	}
-	public Words () {
-		this("");
-	}
+	
 	public String getWord() {
 		return(word);
 	}
+	
 	public int getPoints() {
 		return(points);
 	}
+	
 	public Coord getCoord() {
 		return(coord);
 	}
+	
 	public void downWord () {
 		coord.down();
 	}
 	
-	//inutile
-	public boolean equalsWords(Words w) {
-		return ((this.word.equals(w.word)) && (this.points==w.points) && (this.coord.equalsCoord(w.coord)));
+	public void initCoord() {
+		Random random = new Random();
+		coord = new Coord(random.nextInt(5), 0);
 	}
-	
 }
