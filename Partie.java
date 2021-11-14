b12d9024b77ce6a7f7b6a4e04476de4923d19467
Romane-kulesza <74608881+Romane-kulesza@users.noreply.github.com>
2021-11-14 15:34:32 +0100

    Update Partie.java

--- Partie.java
+++ Partie.java
@@ -24,7 +24,8 @@ public class Partie implements ActionListener{
 	private int score = 0;
 	private Words wordToRemove;
 	private int etape = 0;
-	
+	private int add = 1;
+	private Random r = new Random();
 	
 	public Partie(String ps) {
 		compteur=0;
@@ -38,86 +39,7 @@ public class Partie implements ActionListener{
 		fenetre.setVisible(true);
 		timer.start();
 	}
-	
-	public void saveScore() {
-		String line="";
-		int scoreToSave=this.getScore();
-		Player player=this.getPlayer();
-		String pseudonyme=player.getPseudo();
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(new File ("C:\\Users\\cstav\\eclipse-workspace\\projet\\src\\projet\\abcd.txt")));
-			line = reader.readLine();
-			if(line!=null) {
-				ArrayList<String> pseudo = new ArrayList<String>(Arrays.asList(line.split(",")));
-				line=reader.readLine();
-				ArrayList<String> score  = new ArrayList<String>(Arrays.asList(line.split(",")));
-				
-				BufferedWriter writer = new BufferedWriter(new FileWriter(new File("C:\\Users\\cstav\\eclipse-workspace\\projet\\src\\projet\\abcd.txt")));
-				
-				int j=0;//sauvegarde indice pour ecrire les scores sur deuxieme ligne
-				
-				if(pseudo.size()== 1){ //SI UN SEUL RESULTAT SAUVEGARDE
-					if (Integer.parseInt(score.get(j))<=scoreToSave) {
-						writer.write(pseudonyme+","+pseudo.get(j));
-						writer.newLine();
-						writer.write(Integer.toString(scoreToSave)+","+score.get(j));
-					}
-					else {
-						writer.write(Integer.toString(scoreToSave)+","+score.get(j));
-						writer.newLine();
-						writer.write(pseudonyme+","+pseudo.get(j));
-					}
-				}
-				else { //SI ENTRE 2 ET 10 ELEMENTS DANS LE FICHIER
-					while ((j<score.size()) && Integer.parseInt(score.get(j))>scoreToSave) { //on trouve l'indice où inserer nouveau score ( on incremente j)
-							j++;
-					}
-					if(j<10) {
-						pseudo.add(j,pseudonyme);
-						score.add(j,Integer.toString(scoreToSave));
-						
-						if(pseudo.size()==11) { // ON SUPPRIME L'ELEMENT EN TROP
-							pseudo.remove(10);
-							score.remove(10);
-						}
-					}	
-					//on reecrit toutes les valeurs dans le fichier maintenant que score et pseudo ont les valeurs qu'ils faut 
-					int k=0;
-					while(k<(pseudo.size()-1)){ //on ecrit tous les elements sauf le dernier 
-						writer.write(pseudo.get(k)+",");
-						k++;
-					}
-					writer.write(pseudo.get(pseudo.size()-1)); //on ajoute le dernier element
-					writer.newLine();
-					k=0;
-					while(k<(score.size()-1)){
-						writer.write(score.get(k)+",");
-						k++;
-					}
-					writer.write(score.get(score.size()-1));
-				}
-				writer.close();
-			}
-			else { //SI AUCUN SCORE ENCORE SAUVEGARDE
-				BufferedWriter writer = new BufferedWriter(new FileWriter(new File("C:\\Users\\cstav\\eclipse-workspace\\projet\\src\\projet\\abcd.txt")));
-				writer.write(pseudonyme);
-				writer.newLine();
-				writer.write(scoreToSave);
-				writer.close();
-			}
-				
-			reader.close();	
-			
-			
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
+
 	public void updatePosition(){ 
 		for (Words w : screenWords) {
 	    	if (w.getCoord().getY() == LOWER) {
@@ -128,16 +50,16 @@ public class Partie implements ActionListener{
 	    	else {w.downWord();}
 	    }
 		removeWord(wordToRemove);
-		if (compteur==1){
-    		if( index==(list.getWords().size())-1) {
-//    			list.shuffle();
-    			index=0;
-    		}
-    		Words word = list.getWords().get(index);
-    		word.initCoord();
-    		screenWords.add(word);
-    		compteur=0;
-    		index++;
+		if (compteur==add){
+    			if( index==(list.getWords().size())-1) {
+    				index=0;
+    			}
+			Words word = list.getWords().get(index);
+			word.initCoord();
+			screenWords.add(word);
+			compteur=0;
+			index++;
+			add = r.nextInt(3)+1;
 		}
 		compteur++;
 		if (score>50 && etape < 1){
@@ -201,14 +123,6 @@ public class Partie implements ActionListener{
 	public void removeWord(Words w) {
 		screenWords.remove(w);
 	}
-	
-	/*
-	 * public void end() { //FenetreFin //avec swing et mothode sauvegarde // a
-	 * completer, la partie est terminÃ©e, on est sortie du while verifiant le nbr de
-	 * vie restant, on affiche le temps de jeu, je score, on sauvegarde le score
-	 * etc... //appel a la methode sauvegarde etc... new FenetreFin(player, score);
-	 * }
-	 */
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
