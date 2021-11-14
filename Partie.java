418ab57c9dadd1de5f48950fe3b282408bb67aa9
Carolina-stav <81708703+Carolina-stav@users.noreply.github.com>
2021-11-14 18:13:59 +0100

    Update Partie.java

--- Partie.java
+++ Partie.java
@@ -11,6 +11,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Partie implements ActionListener{
 	private Player player;
