039c249369e85b4b4f3ae1fa92fb1f3d02269509
skander-yousfi <79521109+Skander-Yousfi@users.noreply.github.com>
2021-11-15 15:50:38 +0100

    Update Main.java

--- falling-words-main/Main.java
+++ falling-words-main/Main.java
@@ -1,6 +1,16 @@
 package projet;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Main.
+ */
 public class Main {
+	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main (String [] args) {
 //		Partie p = new Partie("Romane");
 //		Test t = new Test(p);
