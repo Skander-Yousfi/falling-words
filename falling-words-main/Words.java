04b43a75606f8534ae874138a392e7bf420bc811
skander-yousfi <79521109+Skander-Yousfi@users.noreply.github.com>
2021-11-15 15:53:00 +0100

    Update Words.java

--- falling-words-main/Words.java
+++ falling-words-main/Words.java
@@ -2,11 +2,26 @@ package projet;
 
 import java.util.Random;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Words.
+ */
 public class Words {
+	
+	/** The word. */
 	private String word;
+	
+	/** The points. */
 	private int points;
+	
+	/** The coord. */
 	private Coord coord;
 	
+	/**
+	 * Instantiates a new words.
+	 *
+	 * @param mot the mot
+	 */
 	public Words (String mot) {
 		this.word = mot;
 		this.points = mot.length();
@@ -14,28 +29,56 @@ public class Words {
 		this.coord = new Coord(random.nextInt(5), 0);
 	}
 	
+	/**
+	 * Instantiates a new words.
+	 *
+	 * @param mot the mot
+	 * @param x the x
+	 * @param y the y
+	 */
 	public Words (String mot, int x, int y) {
 		this.word = mot;
 		this.points = mot.length();
 		this.coord = new Coord(x,y);
 	}
 	
+	/**
+	 * Gets the word.
+	 *
+	 * @return the word
+	 */
 	public String getWord() {
 		return(word);
 	}
 	
+	/**
+	 * Gets the points.
+	 *
+	 * @return the points
+	 */
 	public int getPoints() {
 		return(points);
 	}
 	
+	/**
+	 * Gets the coord.
+	 *
+	 * @return the coord
+	 */
 	public Coord getCoord() {
 		return(coord);
 	}
 	
+	/**
+	 * Down word.
+	 */
 	public void downWord () {
 		coord.down();
 	}
 	
+	/**
+	 * Inits the coord.
+	 */
 	public void initCoord() {
 		Random random = new Random();
 		coord = new Coord(random.nextInt(5), 0);
