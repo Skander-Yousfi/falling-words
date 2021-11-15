faf9dab77045239ced2fb819216f83ab831a9453
skander-yousfi <79521109+Skander-Yousfi@users.noreply.github.com>
2021-11-15 14:45:17 +0100

    Update CoordTest.java

--- CoordTest.java
+++ CoordTest.java
@@ -1,10 +1,7 @@
-package test;
+package projet;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import projet.Coord;
 
 /**
  * The Class CoordTest.
