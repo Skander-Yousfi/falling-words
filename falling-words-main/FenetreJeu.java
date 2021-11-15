b293a92711ce3481b66a4a0e777d39bf5b918a0c
skander-yousfi <79521109+Skander-Yousfi@users.noreply.github.com>
2021-11-15 15:50:20 +0100

    Update FenetreJeu.java

--- falling-words-main/FenetreJeu.java
+++ falling-words-main/FenetreJeu.java
@@ -4,20 +4,48 @@ import java.awt.*;
 import java.util.ArrayList;
 import javax.swing.* ;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class FenetreJeu.
+ */
 public class FenetreJeu extends JFrame {
+	
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
+	
+	/** The text. */
 	private JTextField text;
+	
+	/** The score. */
 	private JLabel score;
+	
+	/** The s. */
 	private JPanel s;
+	
+	/** The health. */
 	private JLabel health;
+	
+	/** The h. */
 	private JPanel h;
+	
+	/** The panel. */
 	private JPanel panel;
+	
+	/** The pane. */
 	private JPanel pane;
+	
+	/** The c. */
 	private GridBagConstraints c;
 	
+	/**
+	 * Instantiates a new fenetre jeu.
+	 *
+	 * @param l the l
+	 * @param p the p
+	 */
 	public FenetreJeu(ArrayList<Words> l, Player p) {
 		super("Falling Words");
-		setSize(450, 400);
+		setSize(450, 450);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		panel = new JPanel(); 
@@ -60,9 +88,22 @@ public class FenetreJeu extends JFrame {
 		panel.add(text, Component.CENTER_ALIGNMENT);
 		setContentPane(panel);
 	}
+	
+	/**
+	 * Gets the text field.
+	 *
+	 * @return the text field
+	 */
 	public JTextField getTextField() {
 		return(text);
 	}
+	
+	/**
+	 * Creates the tab.
+	 *
+	 * @param l the l
+	 * @return the words[][]
+	 */
 	public Words[][] createTab(ArrayList<Words> l){
 		Words[][] list = new Words[5][15];
 		for (int i=0; i<5; i++) {
@@ -75,6 +116,12 @@ public class FenetreJeu extends JFrame {
 		}
 		return(list);
 	}
+	
+	/**
+	 * Update screen.
+	 *
+	 * @param l the l
+	 */
 	public void updateScreen(ArrayList<Words> l) {
 		pane.removeAll();
 		Words[][] list = createTab(l);
@@ -84,7 +131,6 @@ public class FenetreJeu extends JFrame {
 				c.gridx = w.getCoord().getX();
 				c.gridy = w.getCoord().getY();
 				if (w.getWord()!="                     ") {
-//					t.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.red),pane.getBorder()));
 					t.setBackground(Color.gray);
 				}
 				else {
@@ -97,11 +143,23 @@ public class FenetreJeu extends JFrame {
 		pane.validate();
 		pane.repaint();
 	}
+	
+	/**
+	 * Update health.
+	 *
+	 * @param p the p
+	 */
 	public void updateHealth(Player p) {
 		health.setText("Nombre de vies : "+p.getHealth());
 		h.validate();
 		h.repaint();
 	}
+	
+	/**
+	 * Update score.
+	 *
+	 * @param score the score
+	 */
 	public void updateScore(int score) {
 		this.score.setText("Score : "+ score);
 		s.validate();
