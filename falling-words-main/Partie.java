c329a78100be27a71b0010bda18162781476919b
skander-yousfi <79521109+Skander-Yousfi@users.noreply.github.com>
2021-11-15 15:50:57 +0100

    Update Partie.java

--- falling-words-main/Partie.java
+++ falling-words-main/Partie.java
@@ -3,31 +3,58 @@ package projet;
 import java.awt.event.*;  
 import java.util.ArrayList;
 import javax.swing.Timer;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.Random;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Partie.
+ */
 public class Partie implements ActionListener{
+	
+	/** The player. */
 	private Player player;
+	
+	/** The list. */
 	private WordList list;
+	
+	/** The compteur. */
 	private int compteur; 
+	
+	/** The screen words. */
 	private ArrayList<Words> screenWords; 
+	
+	/** The lower. */
 	final int LOWER = 14; // ordonnee la plus basse a ne pas depasser => utile???
+	
+	/** The index. */
 	private int index = 0; //pour parcourir notre tableau de base de mots 
+	
+	/** The fenetre. */
 	private FenetreJeu fenetre;
+	
+	/** The timer. */
 	private Timer timer;
+	
+	/** The score. */
 	private int score = 0;
+	
+	/** The word to remove. */
 	private Words wordToRemove;
+	
+	/** The etape. */
 	private int etape = 0;
+	
+	/** The add. */
 	private int add = 1;
+	
+	/** The r. */
 	private Random r = new Random();
 	
+	/**
+	 * Instantiates a new partie.
+	 *
+	 * @param ps the ps
+	 */
 	public Partie(String ps) {
 		compteur=0;
 		list= new WordList();
@@ -41,6 +68,9 @@ public class Partie implements ActionListener{
 		timer.start();
 	}
 
+	/**
+	 * Update position.
+	 */
 	public void updatePosition(){ 
 		for (Words w : screenWords) {
 	    	if (w.getCoord().getY() == LOWER) {
@@ -107,6 +137,12 @@ public class Partie implements ActionListener{
 		}
 	}
 	
+	/**
+	 * Verif word.
+	 *
+	 * @param word the word
+	 * @return the words
+	 */
 	public Words verif_word(String word) {
 		Words res = null;
 		for (Words w:screenWords) {
@@ -117,31 +153,66 @@ public class Partie implements ActionListener{
 		return(res);
 	}
 	
+	/**
+	 * Adds the to score.
+	 *
+	 * @param a the a
+	 */
 	public void addToScore(int a) {
 		score += a;
 	}
 	
+	/**
+	 * Removes the word.
+	 *
+	 * @param w the w
+	 */
 	public void removeWord(Words w) {
 		screenWords.remove(w);
 	}
 	
+	/**
+	 * Action performed.
+	 *
+	 * @param e the e
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		updatePosition();
 	}
 	
+	/**
+	 * Gets the frame.
+	 *
+	 * @return the frame
+	 */
 	public FenetreJeu getFrame() {
 		return(fenetre);
 	}
 	
+	/**
+	 * Gets the player.
+	 *
+	 * @return the player
+	 */
 	public Player getPlayer() {
 		return(player);
 	}
 	
+	/**
+	 * Gets the score.
+	 *
+	 * @return the score
+	 */
 	public int getScore() {
 		return(score);
 	}
 	
+	/**
+	 * Gets the screen words.
+	 *
+	 * @return the screen words
+	 */
 	public ArrayList<Words> getScreenWords(){
 		return(screenWords);
 	}
