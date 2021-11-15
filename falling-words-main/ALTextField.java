c16d9c4a9f78479ee44f31f5b64cebb4780d5a02
skander-yousfi <79521109+Skander-Yousfi@users.noreply.github.com>
2021-11-15 15:47:18 +0100

    Update ALTextField.java

--- falling-words-main/ALTextField.java
+++ falling-words-main/ALTextField.java
@@ -2,21 +2,43 @@ package projet;
 
 import java.awt.event.*;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ALTextField.
+ */
 public class ALTextField implements KeyListener{
 	
+	/** The p. */
 	private Partie p;
+	
+	/** The f. */
 	private FenetreJeu f;
 	
+	/**
+	 * Instantiates a new AL text field.
+	 *
+	 * @param p the p
+	 */
 	public ALTextField(Partie p) {
 		this.p = p;
 		f = p.getFrame();
 		f.getTextField().addKeyListener(this);
 	}
 
+	/**
+	 * Key typed.
+	 *
+	 * @param e the e
+	 */
 	@Override
 	public void keyTyped(KeyEvent e) {
 	}
 
+	/**
+	 * Key pressed.
+	 *
+	 * @param e the e
+	 */
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode()== 32 ) {
@@ -31,6 +53,11 @@ public class ALTextField implements KeyListener{
 		}
 	}
 
+	/**
+	 * Key released.
+	 *
+	 * @param e the e
+	 */
 	@Override
 	public void keyReleased(KeyEvent e) {
 	}
