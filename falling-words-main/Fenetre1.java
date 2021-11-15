b458c8ec768a3d05d25403e206c03192dbd466e5
skander-yousfi <79521109+Skander-Yousfi@users.noreply.github.com>
2021-11-15 15:49:19 +0100

    Update Fenetre1.java

--- falling-words-main/Fenetre1.java
+++ falling-words-main/Fenetre1.java
@@ -8,19 +8,45 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Fenetre1.
+ */
 public class Fenetre1 implements ActionListener{
+	
+	/** The f. */
 	private JFrame f;
+	
+	/** The img. */
 	private ImageIcon img = new ImageIcon("img.jpeg");
+	
+	/** The button. */
 	private JButton button = new JButton("Jouer");
+	
+	/** The t. */
 	private JTextField t = new JTextField("Pseudonyme");
+	
+	/** The label. */
 	private JLabel label = new JLabel("Entrez votre pseudonyme pour jouer : ");
+	
+	/** The icone. */
 	private JLabel icone = new JLabel(img, JLabel.CENTER);
+	
+	/** The conteneur. */
 	private JPanel conteneur = new JPanel();
 	
+	/**
+	 * Gets the frame.
+	 *
+	 * @return the frame
+	 */
 	public JFrame getFrame () {
 		return(f);
 	}
 	
+	/**
+	 * Instantiates a new fenetre 1.
+	 */
 	public Fenetre1() {
 		f = new JFrame("Ma fenetre");
 		f.setSize(400, 300);
@@ -34,6 +60,12 @@ public class Fenetre1 implements ActionListener{
 		f.setContentPane(conteneur);
 		f.setVisible(true);
 	}
+	
+	/**
+	 * Action performed.
+	 *
+	 * @param evt the evt
+	 */
 	public void actionPerformed(ActionEvent evt) {
 		  new Partie(t.getText());
 		  f.dispose();
